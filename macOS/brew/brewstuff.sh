43298bb05581f3e6bab0f5a1b512f8b1cd113403
Seth Grover <Seth.D.Grover@gmail.com>
2019-02-18 10:53:25 -0700

    added non-sensitive configs

---
+++ macOS/brew/brewstuff.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+pushd /usr/local/bin
+
+brew cask install diskwave
+brew cask install docker-edge
+brew cask install firefox-beta
+brew cask install google-chrome-beta
+brew cask install homebrew/cask-fonts/font-hack
+brew cask install homebrew/cask-versions/firefox-beta
+brew cask install iterm2
+brew cask install keepassxc
+brew cask install osxfuse
+brew cask install sublime-text
+brew cask install vlc
+brew cask install wireshark
+brew install bash
+brew install bash-completion
+brew install binutils
+brew install bro
+brew install cmake
+brew install coreutils --with-default-names
+brew install diffutils
+brew install dos2unix
+brew install ed --with-default-names
+brew install findutils --with-default-names
+brew install gawk
+brew install git
+brew install gnu-indent --with-default-names
+brew install gnu-sed --with-default-names
+brew install gnu-tar --with-default-names
+brew install gnu-which --with-default-names
+brew install gnutls
+brew install grep --with-default-names
+brew install gzip
+brew install htop
+brew install iproute2mac
+brew install less
+brew install openssh
+brew install p7zip
+brew install pigz
+brew install psgrep
+brew install psutils --with-default-names
+brew install pyenv
+brew install screen
+brew install tree
+brew install unrar
+brew install unzip
+brew install vim --override-system-vi
+brew install watch
+brew install wdiff --with-gettext
+brew install wget
+brew install youtube-dl mpv ffmpeg
+
+popd
---
+++ macOS/brew/gnu_coreutils_links.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+pushd /usr/local/bin
+
+sudo ln -s -f gchmod chmod
+sudo ln -s -f gchown chown
+sudo ln -s -f gcp cp
+sudo ln -s -f gcut cut
+sudo ln -s -f gcut cut
+sudo ln -s -f gcut cut
+sudo ln -s -f gdate date
+sudo ln -s -f gdf df
+sudo ln -s -f gdf df
+sudo ln -s -f gdu du
+sudo ln -s -f gecho echo
+sudo ln -s -f genv env
+sudo ln -s -f ghead head
+sudo ln -s -f gkill kill
+sudo ln -s -f gln ln
+sudo ln -s -f gls ls
+sudo ln -s -f gmv mv
+sudo ln -s -f gprintf printf
+sudo ln -s -f gpwd pwd
+sudo ln -s -f grm rm
+sudo ln -s -f grmdir rmdir
+sudo ln -s -f gshred shred
+sudo ln -s -f gsleep sleep
+sudo ln -s -f gsort sort
+sudo ln -s -f gsplit split
+sudo ln -s -f gstat stat
+sudo ln -s -f gtail tail
+sudo ln -s -f gtee tee
+sudo ln -s -f gtee tee
+sudo ln -s -f gtouch touch
+sudo ln -s -f gtr tr
+sudo ln -s -f guniq uniq
+sudo ln -s -f gwhoami whoami
+
+popd
