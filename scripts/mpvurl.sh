2fcb07ee366e042f28f6a7747fba150e55b20638
SG <13872653+mmguero@users.noreply.github.com>
2019-12-04 13:33:57 -0700

    primary selection

--- scripts/mpvurl.sh
+++ scripts/mpvurl.sh
@@ -2,6 +2,6 @@
 
 [[ -r "$HOME"/.bashrc.d/04_envs.bashrc ]] && . "$HOME"/.bashrc.d/04_envs.bashrc
 
-URL="$(xsel -bo 2>/dev/null)"
+URL="$(xsel -o 2>/dev/null)"
 URL_LENGTH=${#URL}
 [[ -n $URL ]] && (( $URL_LENGTH > 6 )) && mpv "$URL"
