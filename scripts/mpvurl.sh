b62232494e1943e8982b50c27f66c318e9fcdbb3
SG <13872653+mmguero@users.noreply.github.com>
2019-12-04 13:26:18 -0700

    hacky

--- scripts/mpvurl.sh
+++ scripts/mpvurl.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+[[ -r "$HOME"/.bashrc.d/04_envs.bashrc ]] && . "$HOME"/.bashrc.d/04_envs.bashrc
+
 URL="$(xsel -bo 2>/dev/null)"
 URL_LENGTH=${#URL}
 [[ -n $URL ]] && (( $URL_LENGTH > 6 )) && mpv "$URL"
