3d0d88c641a463ca30a8210ae8173b750e62cd1d
SG <13872653+mmguero@users.noreply.github.com>
2020-03-16 22:11:26 -0600

    install git/curl

--- linux/vagrant/windows10/WSL_Debian_Setup.ps1
+++ linux/vagrant/windows10/WSL_Debian_Setup.ps1
@@ -2,5 +2,5 @@ Start-Process "debian.exe" -ArgumentList "install --root" -Wait
 Start-Process "debian.exe" -ArgumentList 'run adduser vagrant --gecos ",,,," --disabled-password' -Wait
 Start-Process "debian.exe" -ArgumentList "run echo 'vagrant:vagrant' | sudo chpasswd" -Wait
 Start-Process "debian.exe" -ArgumentList "run usermod -aG sudo vagrant" -Wait
+Start-Process "debian.exe" -ArgumentList "run apt-get update && apt-get -y dist-upgrade && apt-get -y install curl git" -Wait
 Start-Process "debian.exe" -ArgumentList "config --default-user vagrant" -Wait
-
