b0232c1e8dde661739f3057c92851eab4c13b15e
SG <13872653+mmguero@users.noreply.github.com>
2020-12-27 00:39:49 -0700

    ldap updates

---
+++ services/openldap/openldap_renew_certs_and_restart.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# requires that openldap image be built with UID/GID so that the runtime
+# user has permissions to the cert files. also, for now the user needs to
+# be able to run sudo without prompt (todo)
+
+set -e
+
+STEP_CA_FINGERPRINT="xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+
+CERTS_DIR="certs"
+CRT_NAME="openldap.crt"
+KEY_NAME="openldap.key"
+CA_NAME="ca.crt"
+
+LDAP_CERTS_DIR="certs_ldap"
+LDAP_CRT_NAME="ldap.crt"
+LDAP_KEY_NAME="ldap.key"
+LDAP_CA_NAME="ca.crt"
+
+WEB_CERTS_DIR="certs_web"
+WEB_CRT_NAME="phpldapadmin.crt"
+WEB_KEY_NAME="phpldapadmin.key"
+WEB_CA_NAME="ca.crt"
+
+EXPORT_LDIF_NAME="export.ldif"
+EXPORT_WAIT_SECS=60
+LDAP_ADMIN_CN="cn=admin,dc=example,dc=org"
+LDAP_ADMIN_URL="ldap://localhost:389"
+
+# force-navigate to Malcolm base directory (parent of scripts/ directory)
+RUN_PATH="$(pwd)"
+[[ "$(uname -s)" = 'Darwin' ]] && REALPATH=grealpath || REALPATH=realpath
+[[ "$(uname -s)" = 'Darwin' ]] && DIRNAME=gdirname || DIRNAME=dirname
+if ! (type "$REALPATH" && type "$DIRNAME" && type step && type ldapadd) > /dev/null; then
+  echo "$(basename "${BASH_SOURCE[0]}") requires $REALPATH, $DIRNAME, step and ldapadd"
+  exit 1
+fi
+SCRIPT_PATH="$($DIRNAME $($REALPATH -e "${BASH_SOURCE[0]}"))"
+pushd "$SCRIPT_PATH" >/dev/null 2>&1
+
+pushd ./"$CERTS_DIR" >/dev/null 2>&1
+rm -f ./"$CA_NAME"
+step ca root ./"$CA_NAME" --fingerprint "$STEP_CA_FINGERPRINT"
+step ca renew --force "$CRT_NAME" "$KEY_NAME"
+popd >/dev/null 2>&1
+
+docker-compose down || true
+
+pushd ./"$LDAP_CERTS_DIR" >/dev/null 2>&1
+cp ../"$CERTS_DIR"/"$CA_NAME" ./"$LDAP_CA_NAME"
+cp ../"$CERTS_DIR"/"$CRT_NAME" ./"$LDAP_CRT_NAME"
+cp ../"$CERTS_DIR"/"$KEY_NAME" ./"$LDAP_KEY_NAME"
+popd >/dev/null 2>&1
+
+pushd ./"$WEB_CERTS_DIR" >/dev/null 2>&1
+sudo cp ../"$CERTS_DIR"/"$CA_NAME" ./"$WEB_CA_NAME"
+sudo cp ../"$CERTS_DIR"/"$CRT_NAME" ./"$WEB_CRT_NAME"
+sudo cp ../"$CERTS_DIR"/"$KEY_NAME" ./"$WEB_KEY_NAME"
+popd >/dev/null 2>&1
+
+docker-compose up -d
+
+if [[ -r "$EXPORT_LDIF_NAME" ]]; then
+  sleep $EXPORT_WAIT_SECS
+  ldapadd -x -D "$LDAP_ADMIN_CN" -w "$(grep LDAP_ADMIN_PASSWORD docker-compose.yml | sed "s/.*[[:space:]]*=[[:space:]]*//")" -H "$LDAP_ADMIN_URL" -f "$EXPORT_LDIF_NAME"
+fi
