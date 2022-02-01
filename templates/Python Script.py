8bd81e1d94de5a645978e9b508c7b4ce74df77e6
SG <13872653+mmguero@users.noreply.github.com>
2022-02-01 15:04:23 -0700

    update template

--- templates/Python Script.py
+++ templates/Python Script.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import logging
 import os
 import sys
 
@@ -9,7 +10,6 @@ import mmguero
 
 ###################################################################################################
 args = None
-debug = False
 script_name = os.path.basename(__file__)
 script_path = os.path.dirname(os.path.realpath(__file__))
 orig_path = os.getcwd()
@@ -17,32 +17,64 @@ orig_path = os.getcwd()
 ###################################################################################################
 # main
 def main():
-  global args
-  global debug
+    global args
 
-  parser = argparse.ArgumentParser(description=script_name, add_help=False, usage='{} <arguments>'.format(script_name))
-  parser.add_argument('-d', '--defaults', dest='accept_defaults', type=mmguero.str2bool, nargs='?', const=True, default=False, metavar='true|false', help="Accept defaults to prompts without user interaction")
-  parser.add_argument('-v', '--verbose', dest='debug', type=mmguero.str2bool, nargs='?', const=True, default=False, metavar='true|false', help="Verbose/debug output")
-  parser.add_argument('-i', '--input', dest='input', type=str, default=None, required=False, metavar='<string>', help="Input")
-  try:
-    parser.error = parser.exit
-    args = parser.parse_args()
-  except SystemExit:
-    parser.print_help()
-    exit(2)
+    parser = argparse.ArgumentParser(
+        description='\n'.join(
+            [
+                'Do some stuff.',
+            ]
+        ),
+        formatter_class=argparse.RawTextHelpFormatter,
+        add_help=False,
+        usage='{} <arguments>'.format(script_name),
+    )
+    parser.add_argument('--verbose', '-v', action='count', default=1, help='Increase verbosity (e.g., -v, -vv, etc.)')
+    parser.add_argument(
+        '--foo',
+        dest='foo',
+        action='store_true',
+        help='Foo',
+    )
+    parser.add_argument(
+        '--no-foo',
+        dest='foo',
+        action='store_false',
+        help='Do not foo',
+    )
+    parser.set_defaults(foo=True)
+    parser.add_argument(
+        '-i',
+        '--input',
+        dest='input',
+        nargs='*',
+        type=str,
+        default=None,
+        required=False,
+        help="Input value(s)",
+    )
+    try:
+        parser.error = parser.exit
+        args = parser.parse_args()
+    except SystemExit:
+        parser.print_help()
+        exit(2)
 
-  debug = args.debug
-  if debug:
-    eprint(os.path.join(script_path, script_name))
-    eprint("Arguments: {}".format(sys.argv[1:]))
-    eprint("Arguments: {}".format(args))
-  else:
-    sys.tracebacklimit = 0
+    args.verbose = logging.ERROR - (10 * args.verbose) if args.verbose > 0 else 0
+    logging.basicConfig(
+        level=args.verbose, format='%(asctime)s %(levelname)s: %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+    )
+    logging.debug(os.path.join(script_path, script_name))
+    logging.debug("Arguments: {}".format(sys.argv[1:]))
+    logging.debug("Arguments: {}".format(args))
+    if args.verbose > logging.DEBUG:
+        sys.tracebacklimit = 0
 
-  if args.input is not None:
-    cmd_code, cmd_output = mmguero.RunProcess(args.input)
-    print(f"{cmd_code}: {cmd_output}")
+    if args.input is not None:
+        for val in args.input:
+            logging.info(val)
+
 
 ###################################################################################################
 if __name__ == '__main__':
-  main()
+    main()
