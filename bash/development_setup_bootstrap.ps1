033944ba44f1761a40b3b2a0054eff9bc7f916be
SG <mero.mero.guero@gmail.com>
2022-09-07 11:22:55 -0600

    fix bootsrap

--- bash/development_setup_bootstrap.ps1
+++ bash/development_setup_bootstrap.ps1
@@ -8,8 +8,8 @@ iwr -useb get.scoop.sh -outfile 'install_scoop.ps1'
 Remove-Item .\install_scoop.ps1
 
 # bootstrap bare minimum (let msys and/or development_setup.sh do the rest)
-scoop bucket add mmguero https://github.com/mmguero/scoop-bucket
 scoop install main/msys2 main/git main/ln
+scoop bucket add mmguero https://github.com/mmguero/scoop-bucket
 
 # enable permission to create symlinks
 # from https://dbondarchuk.com/2016/09/23/adding-permission-for-creating-symlink-using-powershell/
