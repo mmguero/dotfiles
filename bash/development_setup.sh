96087e4cef85c71c9ba5a434eba833959621bc44
SG <mero.mero.guero@gmail.com>
2023-09-18 11:45:37 -0600

    added signing keys

--- bash/development_setup.sh
+++ bash/development_setup.sh
@@ -597,7 +597,7 @@ function SetupAptSources {
           "https://download.docker.com/linux/debian/gpg|/usr/share/keyrings/docker-archive-keyring.gpg"
           "https://download.sublimetext.com/sublimehq-pub.gpg|/usr/share/keyrings/sublimetext-keyring.gpg"
           "https://packages.microsoft.com/keys/microsoft.asc|/usr/share/keyrings/microsoft.gpg"
-          "https://mirrorcache-us.opensuse.org/repositories/home:/alvistack/Debian_12/Release.key|/usr/share/keyrings/home_alvistack.gpg"
+          "https://build.opensuse.org/projects/home:alvistack/signing_keys/download?kind=gpg|/usr/share/keyrings/home_alvistack.gpg"
           "https://packages.fluentbit.io/fluentbit.key|/usr/share/keyrings/fluentbit-keyring.gpg"
           "deb:fasttrack-archive-keyring"
         )
